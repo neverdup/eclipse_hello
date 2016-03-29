06380919fe12c9a095445a00cb13087afe85b13c
neverdup <neverdup@gmail.com>
2016-03-29 11:53:08 +0900

    GitHub for git pull

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -6,6 +6,8 @@ public class PushPrint {
 		System.out.println("Add Java Class File Push");
 		System.out.println("World with 48");
 		System.out.println("Local Change");
+		System.out.println("GitHub Change for pull");
+		
 		
 	}
 
